28c05e00b498704f5666cbcc7dae49ce41e20e2d
TheItsNameless <itsnameles@outlook.com>
2025-10-14 10:42:11 +0200

    Mark multiple and single container tests as explicit to control test execution

--- test/ItsNameless.HPort.IntegrationTest/SingleContainerTest.cs
+++ test/ItsNameless.HPort.IntegrationTest/SingleContainerTest.cs
@@ -4,7 +4,7 @@ namespace ItsNameless.HPort.IntegrationTest;
 [TestOf(typeof(HPort))]
 public class SingleContainerTest : TestBase
 {
-    [Test, Order(1),]
+    [Test, Order(1), Explicit,]
     public async Task
         AddContainer_NoServerExists_CreatesServer_AndAddsContainer()
     {
@@ -56,10 +56,11 @@ public class SingleContainerTest : TestBase
                 Is.EqualTo(_testConfig.Container.Server.Datacenter)
             );
         }
+
         await Log("Test completed.");
     }
 
-    [Test, Order(2),]
+    [Test, Order(2), Explicit,]
     public async Task CheckContainer_RunsCommandInContainer_AndReturnsResult()
     {
         // Act
@@ -67,12 +68,13 @@ public class SingleContainerTest : TestBase
         await Task.Delay(5000);
 
         await Log("Executing command in container...");
-        var result = await _sut.Container.ExecuteCommandInContainer(
-            _testConfig.Container.Name,
-            _testConfig.Container.Server.Name,
-            _testConfig.ServiceName,
-            "nginx -v"
-        );
+        var result =
+            await _sut.Container.ExecuteCommandInContainer(
+                _testConfig.Container.Name,
+                _testConfig.Container.Server.Name,
+                _testConfig.ServiceName,
+                "nginx -v"
+            );
         await Log("-> Command executed with result: " + result);
 
         //Assert
@@ -81,16 +83,17 @@ public class SingleContainerTest : TestBase
         await Log("Test completed.");
     }
 
-    [Test, Order(98),]
+    [Test, Order(98), Explicit,]
     public async Task DeleteContainer_DeletesContainer_AndServer()
     {
         // Act
         await Log("Deleting container...");
-        var actualContainer = await _sut.Container.DeleteContainer(
-            _testConfig.Container.Name,
-            _testConfig.Container.Server.Name,
-            true
-        );
+        var actualContainer =
+            await _sut.Container.DeleteContainer(
+                _testConfig.Container.Name,
+                _testConfig.Container.Server.Name,
+                true
+            );
         await Log("-> Container deleted.");
 
         //Assert
@@ -103,11 +106,15 @@ public class SingleContainerTest : TestBase
             );
             Assert.That(actualContainer.Server, Is.Not.Null);
         }
-        Assert.That(actualContainer.Server.Name, Is.EqualTo(_testConfig.Container.Server.Name));
+
+        Assert.That(
+            actualContainer.Server.Name,
+            Is.EqualTo(_testConfig.Container.Server.Name)
+        );
         await Log("Test completed.");
     }
 
-    [Test, Order(99),]
+    [Test, Order(99), Explicit,]
     public async Task CheckContainer_ServerDeleted_ServerDoesNotExistAnymore()
     {
         // Act
